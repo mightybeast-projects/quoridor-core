936dbd5881b7704268c429709e0281b081b22bb7
MightyBeast <zgpoter@gmail.com>
2021-07-05 16:30:15 +0300

    Added right/left check for player

--- Tests/PlayerMovement/PlayerOnTheWayMovement.cs
+++ Tests/PlayerMovement/PlayerOnTheWayMovement.cs
@@ -31,5 +31,31 @@ namespace Quoridor.Tests.PlayerMovement
             Assert.IsFalse(_board.grid[8, 6].isEmpty);
             Assert.AreEqual(new Vector2(8, 6), _firstPlayer.position);
         }
+
+        [Test]
+        public void PlayerOnTheWayRight()
+        {
+            _firstPlayer.SetPosition(8, 8);
+            _secondPlayer.SetPosition(10, 8);
+            _firstPlayer.MoveRight();
+
+            Assert.IsTrue(_board.grid[8, 8].isEmpty);
+            Assert.IsFalse(_board.grid[10, 8].isEmpty);
+            Assert.IsFalse(_board.grid[12, 8].isEmpty);
+            Assert.AreEqual(new Vector2(12, 8), _firstPlayer.position);
+        }
+
+        [Test]
+        public void PlayerOnTheWayLeft()
+        {
+            _firstPlayer.SetPosition(8, 8);
+            _secondPlayer.SetPosition(6, 8);
+            _firstPlayer.MoveLeft();
+
+            Assert.IsTrue(_board.grid[8, 8].isEmpty);
+            Assert.IsFalse(_board.grid[6, 8].isEmpty);
+            Assert.IsFalse(_board.grid[4, 8].isEmpty);
+            Assert.AreEqual(new Vector2(4, 8), _firstPlayer.position);
+        }
     }
 }
