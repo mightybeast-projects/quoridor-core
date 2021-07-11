802c11e93ebad1af38fb51c42769a676454b5281
MightyBeast <gpoter@ukr.com>
2021-07-11 21:19:13 +0300

    Refactored diagonal movement tests

--- Tests/PlayerMovement/WrongDiagonalMovement.cs
+++ Tests/PlayerMovement/WrongDiagonalMovement.cs
@@ -4,21 +4,8 @@ using NUnit.Framework;
 namespace Quoridor.Tests.PlayerMovement
 {
     [TestFixture]
-    public class DiagonalMovement : Initialization
+    public class WrongDiagonalMovement : Initialization
     {
-        [Test]
-        public void DoNotMoveIfThereIsWallBehindAnotherPlayer()
-        {
-            _firstPlayer.SetPosition(8, 0);
-            _secondPlayer.SetPosition(8, 2);
-            _firstPlayer.PlaceWall(new Vector2(8, 3), new Vector2(10, 3));
-            _firstPlayer.MoveUp();
-
-            Assert.AreEqual(new Vector2(8, 0), _firstPlayer.position);
-            Assert.IsFalse(_board.grid[8, 0].isEmpty);
-            Assert.IsTrue(_board.grid[8, 4].isEmpty);
-        }
-
         [Test]
         public void MovePlayerDiagonallyWithoutWallBehind()
         {
@@ -32,96 +19,6 @@ namespace Quoridor.Tests.PlayerMovement
             _firstPlayer.MoveDiagonallyTopRight();
 
             Assert.AreEqual(new Vector2(8, 0), _firstPlayer.position);
-            Assert.IsFalse(_board.grid[8, 0].isEmpty);
-            Assert.IsTrue(_board.grid[8, 4].isEmpty);
-        }
-
-        [Test]
-        public void MovePlayerDiagonallyTopRightWithWallBehind1()
-        {
-            _firstPlayer.SetPosition(8, 0);
-            _secondPlayer.SetPosition(8, 2);
-            _firstPlayer.PlaceWall(new Vector2(8, 3), new Vector2(10, 3));
-            _firstPlayer.MoveDiagonallyTopRight();
-
-            Assert.AreEqual(new Vector2(10, 2), _firstPlayer.position);
-        }
-
-        [Test]
-        public void MovePlayerDiagonallyTopRightWithWallBehind2()
-        {
-            _firstPlayer.SetPosition(8, 0);
-            _secondPlayer.SetPosition(10, 0);
-            _firstPlayer.PlaceWall(new Vector2(11, 0), new Vector2(11, 2));
-            _firstPlayer.MoveDiagonallyTopRight();
-
-            Assert.AreEqual(new Vector2(10, 2), _firstPlayer.position);
-        }
-
-        [Test]
-        public void MovePlayerDiagonallyBottomRightWithWallBehind1()
-        {
-            _firstPlayer.SetPosition(8, 2);
-            _secondPlayer.SetPosition(10, 2);
-            _firstPlayer.PlaceWall(new Vector2(11, 0), new Vector2(11, 2));
-            _firstPlayer.MoveDiagonallyBottomRight();
-
-            Assert.AreEqual(new Vector2(10, 0), _firstPlayer.position);
-        }
-
-        [Test]
-        public void MovePlayerDiagonallyBottomRightWithWallBehind2()
-        {
-            _firstPlayer.SetPosition(8, 4);
-            _secondPlayer.SetPosition(8, 2);
-            _firstPlayer.PlaceWall(new Vector2(8, 1), new Vector2(10, 1));
-            _firstPlayer.MoveDiagonallyBottomRight();
-
-            Assert.AreEqual(new Vector2(10, 2), _firstPlayer.position);
-        }
-
-        [Test]
-        public void MovePlayerDiagonallyBottomLeftWithWallBehind1()
-        {
-            _firstPlayer.SetPosition(8, 4);
-            _secondPlayer.SetPosition(8, 2);
-            _firstPlayer.PlaceWall(new Vector2(8, 1), new Vector2(6, 1));
-            _firstPlayer.MoveDiagonallyBottomLeft();
-
-            Assert.AreEqual(new Vector2(6, 2), _firstPlayer.position);
-        }
-
-        [Test]
-        public void MovePlayerDiagonallyBottomLeftWithWallBehind2()
-        {
-            _firstPlayer.SetPosition(8, 2);
-            _secondPlayer.SetPosition(6, 2);
-            _firstPlayer.PlaceWall(new Vector2(5, 0), new Vector2(5, 2));
-            _firstPlayer.MoveDiagonallyBottomLeft();
-
-            Assert.AreEqual(new Vector2(6, 0), _firstPlayer.position);
-        }
-
-        [Test]
-        public void MovePlayerDiagonallyTopLeftWithWallBehind1()
-        {
-            _firstPlayer.SetPosition(8, 0);
-            _secondPlayer.SetPosition(8, 2);
-            _firstPlayer.PlaceWall(new Vector2(8, 3), new Vector2(10, 3));
-            _firstPlayer.MoveDiagonallyTopLeft();
-
-            Assert.AreEqual(new Vector2(6, 2), _firstPlayer.position);
-        }
-
-        [Test]
-        public void MovePlayerDiagonallyTopLeftWithWallBehind2()
-        {
-            _firstPlayer.SetPosition(8, 2);
-            _secondPlayer.SetPosition(6, 2);
-            _firstPlayer.PlaceWall(new Vector2(5, 0), new Vector2(5, 2));
-            _firstPlayer.MoveDiagonallyTopLeft();
-
-            Assert.AreEqual(new Vector2(6, 4), _firstPlayer.position);
         }
 
         [Test]
