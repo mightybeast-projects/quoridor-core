56a3129fef492788623521b4c8289e670021d103
MightyBeast <zgpoter@gmail.com>
2021-07-14 14:50:19 +0300

    First implementation of game logic

--- Core/GameLogic/Game.cs
+++ Core/GameLogic/Game.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections.Generic;
+using System.Numerics;
 using Quoridor.Core.PlayerLogic;
+using quoridor_core.Core.GameLogic;
 
 namespace Quoridor.Core.GameLogic
 {
@@ -12,6 +15,7 @@ namespace Quoridor.Core.GameLogic
         private List<Player> _players;
         private Board _board;
         private Player _currentPlayer;
+        private int _currentPlayerIndex;
 
         public Game(Board board, List<Player> players)
         {
@@ -24,13 +28,76 @@ namespace Quoridor.Core.GameLogic
             _players[0].SetGoal(8, 16);
             _players[1].SetGoal(8, 0);
 
-            _currentPlayer = _players[0];
+            _currentPlayer = _players[_currentPlayerIndex];
+        }
+
+        public void MakeCurrentPlayerMove(PlayerMove playerMove)
+        {
+            Vector2 previousPosition = _currentPlayer.position;
+
+            ChooseMove(playerMove);
+
+            if(_currentPlayer.position == previousPosition)
+                return;
+
+            SwitchCurrentPlayer();
+        }
+
+        public void MakeCurrentPlayerPlaceWall(Vector2 wallStartPosition, Vector2 wallEndPosition)
+        {
+            int previousWallCounter = _currentPlayer.wallCounter;
+
+            _currentPlayer.PlaceWall(wallStartPosition, wallEndPosition);
+
+            if(_currentPlayer.wallCounter == previousWallCounter)
+                return;
+
+            SwitchCurrentPlayer();
         }
 
         public void SetPlayersOutput(IOutput output)
         {
             foreach (Player player in _players)
                 player.SetOutput(output);
+        }
+
+        private void SwitchCurrentPlayer()
+        {
+            _currentPlayerIndex++;
+            if (_currentPlayerIndex > _players.Count - 1) 
+                _currentPlayerIndex = 0;
+            _currentPlayer = _players[_currentPlayerIndex];
+        }
+
+        private void ChooseMove(PlayerMove playerMove)
+        {
+            switch (playerMove)
+            {
+                case PlayerMove.MOVE_UP:
+                    _currentPlayer.MoveUp();
+                    break;
+                case PlayerMove.MOVE_DOWN:
+                    _currentPlayer.MoveDown();
+                    break;
+                case PlayerMove.MOVE_RIGHT:
+                    _currentPlayer.MoveRight();
+                    break;
+                case PlayerMove.MOVE_LEFT:
+                    _currentPlayer.MoveLeft();
+                    break;
+                case PlayerMove.MOVE_DIAGONALLY_TOP_RIGHT:
+                    _currentPlayer.MoveDiagonallyTopRight();
+                    break;
+                case PlayerMove.MOVE_DIAGONALLY_TOP_LEFT:
+                    _currentPlayer.MoveDiagonallyTopLeft();
+                    break;
+                case PlayerMove.MOVE_DIAGONALLY_BOTOM_RIGHT:
+                    _currentPlayer.MoveDiagonallyBottomRight();
+                    break;
+                case PlayerMove.MOVE_DIAGONALLY_BOTTOM_LEFT:
+                    _currentPlayer.MoveDiagonallyBottomLeft();
+                    break;
+            }
         }
     }
 }
--- Tests/GameLogic.cs
+++ Tests/GameLogic.cs
@@ -1,12 +1,69 @@
 using System.Numerics;
 using NUnit.Framework;
 using Quoridor.Core;
+using Quoridor.Core.GameLogic;
+using quoridor_core.Core.GameLogic;
 
 namespace Quoridor.Tests
 {
     [TestFixture]
     public class GameLogic : Initialization
     {
-        
+        protected override void SetUp()
+        {
+            base.SetUp();
+            _players.Add(_firstPlayer);
+            _players.Add(_secondPlayer);
+            _game = new Game(_board, _players);
+        }
+
+        [Test]
+        public void SwitchCurrentPlayerAfterCorrectMove()
+        {
+            _game.MakeCurrentPlayerMove(PlayerMove.MOVE_UP);
+
+            Assert.AreEqual(new Vector2(8, 2), _game.players[0].position);
+            Assert.AreEqual(_game.players[1], _game.currentPlayer);
+        }
+
+        [Test]
+        public void SwitchBackToFirstPlayerAfterSecondPlayerMoved()
+        {
+            _game.MakeCurrentPlayerMove(PlayerMove.MOVE_UP);
+
+            Assert.AreEqual(new Vector2(8, 2), _game.players[0].position);
+
+            _game.MakeCurrentPlayerMove(PlayerMove.MOVE_DOWN);
+
+            Assert.AreEqual(new Vector2(8, 14), _game.players[1].position);
+            Assert.AreEqual(_game.players[0], _game.currentPlayer);
+        }
+
+        [Test]
+        public void DoNotSwitchPlayerAfterWrongMove()
+        {
+            _game.MakeCurrentPlayerMove(PlayerMove.MOVE_DOWN);
+
+            Assert.AreEqual(new Vector2(8, 0), _game.players[0].position);
+            Assert.AreEqual(_game.players[0], _game.currentPlayer);
+        }
+
+        [Test]
+        public void SwitchCurrentPlayerAfterCorrectWallPlacement()
+        {
+            _game.MakeCurrentPlayerPlaceWall(new Vector2(0, 1), new Vector2(2, 1));
+
+            Assert.AreEqual(9, _game.players[0].wallCounter);
+            Assert.AreEqual(_game.players[1], _game.currentPlayer);
+        }
+
+        [Test]
+        public void DoNotSwitchPlayerAfterWrongWallPlacement()
+        {
+            _game.MakeCurrentPlayerPlaceWall(new Vector2(-1, 1), new Vector2(2, 1));
+
+            Assert.AreEqual(10, _game.players[0].wallCounter);
+            Assert.AreEqual(_game.players[0], _game.currentPlayer);
+        }
     }
 }
