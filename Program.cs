7b7caac9090b627a2ebee24ef93bbb8da21ae77d
MightyBeast <zgpoter@gmail.com>
2021-07-29 14:44:26 +0300

    Fixed program class after terminal app separation

--- Program.cs
+++ Program.cs
@@ -1,31 +1,14 @@
 ﻿using Quoridor.Core.GameLogic;
-using Quoridor.Terminal;
 
 namespace Quoridor
 {
     class Program
     {
-        private static Game _game;
-        private static ConsoleApp _consoleApp;
-
         public static void Main()
         {
-            GameExample();
-
-            StartConsoleApp();
-        }
-
-        private static void GameExample()
-        {
-            _game = new Game();
-            _game.AddNewPlayerPair();
-            _game.Start();
-        }
-
-        private static void StartConsoleApp()
-        {
-            _consoleApp = new ConsoleApp(_game);
-            _consoleApp.Start();
+            Game game = new Game();
+            game.AddNewPlayerPair();
+            game.Start();
         }
     }
 }
