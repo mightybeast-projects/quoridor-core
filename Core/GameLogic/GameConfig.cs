bbf38cde09f713963cdf982e65c4e0d2f9fa8ea4
MightyBeast <gpoter@ukr.com>
2021-08-07 23:16:20 +0300

    Reintroduced auto properties

--- Core/GameLogic/GameConfig.cs
+++ Core/GameLogic/GameConfig.cs
@@ -7,26 +7,17 @@ namespace Quoridor.Core.GameLogic
     {
         internal List<Player> players => _players;
         internal Board board => _board;
-        internal Player currentPlayer
-        {
-            get => _currentPlayer;
-            set => _currentPlayer = value;
-        }
-        internal int currentPlayerIndex
-        {
-            get => _currentPlayerIndex;
-            set => _currentPlayerIndex = value;
-        }
+        internal Player currentPlayer { get; set; }
+        internal int currentPlayerIndex { get; set; }
 
         private List<Player> _players;
         private Board _board;
-        private Player _currentPlayer;
-        private int _currentPlayerIndex = -1;
 
         public GameConfig()
         {
             _players = new List<Player>();
             _board = new Board();
+            currentPlayerIndex = -1;
         }
 
         internal void AddNewPlayers()
--- Core/Tile.cs
+++ Core/Tile.cs
@@ -6,16 +6,8 @@ namespace Quoridor.Core
 {
     public class Tile : IPathfindingNode
     {
-        public bool isSolid 
-        { 
-            get => _isSolid; 
-            set => _isSolid = value;
-        }
-        public bool isEmpty 
-        {
-            get => _isEmpty; 
-            set => _isEmpty = value;
-        }
+        public bool isSolid { get; set; }
+        public bool isEmpty { get; set; }
         public Vector2 position => _position;
         public List<Tile> preNeighbors => _preNeighbors;
         public List<Tile> neighbors
@@ -23,24 +15,20 @@ namespace Quoridor.Core
             get => _neighbors;
             set => _neighbors = value; 
         }
-        public float f { get => _f; set => _f = value; }
-        public float g { get => _g; set => _g = value; }
-        public float h { get => _h; set => _h = value; }
-        public IPathfindingNode parent { get => _parent; set => _parent = value; }
+        public float f { get; set; }
+        public float g { get; set; }
+        public float h { get; set; }
+        public IPathfindingNode parent { get; set; }
 
-        private bool _isSolid = true;
-        private bool _isEmpty = true;
         private Vector2 _position;
         private List<Tile> _preNeighbors;
         private List<Tile> _neighbors;
-        private float _f;
-        private float _g;
-        private float _h;
-        private IPathfindingNode _parent;
 
         public Tile(Vector2 position)
         {
             _position = position;
+            isSolid = true;
+            isEmpty = true;
 
             _neighbors = new List<Tile>();
             _preNeighbors = new List<Tile>();
